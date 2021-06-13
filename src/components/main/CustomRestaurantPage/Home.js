dfcd6eb42c1626fbfd42ef63db6de2306a0968a6
TheKillerz7 <mstudym13@gmail.com>
2021-06-13 20:21:03 +0700

    refactor: homepage

--- src/components/main/CustomRestaurantPage/Home.js
+++ src/components/main/CustomRestaurantPage/Home.js
@@ -1,5 +1,8 @@
 import decoration from '../../../assets/imgs/background-dec.png'
 import decoration_mobile from '../../../assets/imgs/background-dec-mobile.png'
+import background_1 from '../../../assets/imgs/background-1.png'
+import restaurant from '../../../assets/imgs/restaurant.png'
+import character from '../../../assets/imgs/character.png'
 import React, { useState, useEffect } from 'react'
 
 const Home = ({ index, setIndex, customHeight }) => {
@@ -17,11 +20,19 @@ const Home = ({ index, setIndex, customHeight }) => {
 
     return(
         <div className={"home-container lg:h-auto pb-10 transition-all duration-300 " + (index == "home" && "delay-300")} style={{ height: `${customHeight?.current?.offsetHeight}px`, maxHeight: (styleObject[index]) }}>
-            <img className="lg:block w-full object-cover hidden left-0 -top-5 absolute" src={decoration} style={{ zIndex: '-1' }} />
+            <img className="lg:block w-full object-cover hidden left-0 -top-5 absolute" className="absolute top-0 laft-0" src={decoration} style={{ zIndex: '-1' }} />
             <img className="lg:hidden h-80 object-cover left-1/2 transform -translate-x-1/2 top-5 absolute" src={decoration_mobile} />
             <div className="xl:px-10 lg:grid lg:grid-cols-2 lg:pt-20 lg:gap-16 lg:px-20 px-5 mx-auto relative" style={{ maxWidth: "1000px" }}>
                 <div className="lg:p-0 pt-10 pb-5 mb-5 z-10">
-                    <div className="lg:w-96 lg:h-96 rounded-full mx-auto w-72 h-72 bg-gray-200"></div>
+                    <div className="lg:w-96 lg:h-96 lg:bg-green-900 lg:bg-opacity-10 relative rounded-full flex mx-auto w-80 h-80">
+                        <div className="lg:hidden absolute overflow-hidden rounded-full top-3 left-0 w-full h-full">
+                            <img className="absolute pb w-full top-0 left-0" src={background_1} />
+                            <img className="absolute pb w-full -bottom-10 -left-48" src={restaurant} />
+                            <img className="absolute pb h-4/6 bottom-0 left-20" src={character} />
+                        </div>
+                        <div className="lg:text-8xl lg:p-0 pl-10 text-7xl pt-3 text-gray-500">3</div>
+                        <div className="lg:text-2xl text-xl text-gray-400 font-medium">ขั้นตอนง่าย ๆ<br/>ในการเปิดแฟรนไชส์</div>
+                    </div>
                 </div>
                 <div className="lg:flex lg:flex-col lg:justify-end lg:pb-16">
                     <p className="lg:text-sm text-xs text-gray-400 mb-7">Lorem iqrun isd rqu rudct<br/>Lorem iqrun isd rqu rudctLorem iqrun isd rqu rudct</p>
